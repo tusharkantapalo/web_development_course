5d51492e3c39317f11e97577a9ed0b83497ab3fd
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-07-24 20:30:14 +0530

    added codes of getElementsByClassName, getElementById, getElementsByTagName and querySelector

--- JavaScript/DOM/DOM Challenge Starting Files/index.js
+++ JavaScript/DOM/DOM Challenge Starting Files/index.js
@@ -1,4 +1,17 @@
+//querySelector()
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
-//querySelecter("") - finds the FIRST query by name at that label
-//getElementsByTagName("") - finds all the queries by name at that label and it returns an array, for which we have to provide the index to which we want to make any change regarding style
-//like - document.getElementsByName("li")[0].style.color = "purple";, here the [] contains the index
+
+//getElementsByTagName()
+document.firstElementChild.lastElementChild.getElementsByClassName("Ramayan")[0].getElementsByTagName("li")[0].style.color = "red";
+
+//getElementsByClassName()
+document.firstElementChild.lastElementChild.getElementsByClassName("Ramayan")[0].getElementsByTagName("li")[0].style.color = "red";
+
+//getElementById()
+document.getElementById("Sita").getElementsByTagName("li")[2].style.color = "red";  //IMPORTANT
+//querySelecter("selector" or "#id" or".class") - finds the FIRST query by name at that label
+            //if we want to add any class or id at the place of selector name, then we can use # or . notation
+//getElementsByTagName("tag") - finds all the queries by name at that label and it returns an array, for which we have to provide the index to which we want to make any change regarding style
+            //like - document.getElementsByName("li")[0].style.color = "purple";, here the [] contains the index
+//getElementsByClassName("class") - finds all the queries by name at that label and it returns an array
+//getElementById("id") - returns a single item not an array
