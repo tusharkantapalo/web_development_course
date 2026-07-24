814e17f00d24c518f752c1f9d61bc30da1e0e12f
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-07-24 20:44:10 +0530

    added combining selectors

--- JavaScript/DOM/DOM Challenge Starting Files/index.js
+++ JavaScript/DOM/DOM Challenge Starting Files/index.js
@@ -9,6 +9,10 @@ document.firstElementChild.lastElementChild.getElementsByClassName("Ramayan")[0]
 
 //getElementById()
 document.getElementById("Sita").getElementsByTagName("li")[2].style.color = "red";  //IMPORTANT
+
+//combining selectors
+document.querySelector("li a").innerHTML = "Chiku";
+
 //querySelecter("selector" or "#id" or".class") - finds the FIRST query by name at that label
             //if we want to add any class or id at the place of selector name, then we can use # or . notation
 //getElementsByTagName("tag") - finds all the queries by name at that label and it returns an array, for which we have to provide the index to which we want to make any change regarding style
