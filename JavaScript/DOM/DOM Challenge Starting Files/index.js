d8f1b86b115ca3ebc1f5a70df3b8b02bf6e30990
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-07-24 21:15:17 +0530

    added querySelectorAll

--- JavaScript/DOM/DOM Challenge Starting Files/index.js
+++ JavaScript/DOM/DOM Challenge Starting Files/index.js
@@ -13,9 +13,13 @@ document.getElementById("Sita").getElementsByTagName("li")[2].style.color = "red
 //combining selectors
 document.querySelector("li a").innerHTML = "Chiku";
 
+//querySelectorAll()
+document.querySelectorAll("li.items")[1].style.color = "red";
+
 //querySelecter("selector" or "#id" or".class") - finds the FIRST query by name at that label
             //if we want to add any class or id at the place of selector name, then we can use # or . notation
 //getElementsByTagName("tag") - finds all the queries by name at that label and it returns an array, for which we have to provide the index to which we want to make any change regarding style
             //like - document.getElementsByName("li")[0].style.color = "purple";, here the [] contains the index
 //getElementsByClassName("class") - finds all the queries by name at that label and it returns an array
 //getElementById("id") - returns a single item not an array
+//querySelectorAll("combined selectors") - it modifies all the elements with the same selection and it returns an array
