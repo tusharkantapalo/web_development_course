2a2c9bf88efcf254c06ef7c6e687621b5fe91974
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-07-24 19:39:05 +0530

    added getElementsByTagName() and querySelector()

--- JavaScript/DOM/DOM Challenge Starting Files/index.js
+++ JavaScript/DOM/DOM Challenge Starting Files/index.js
@@ -1,1 +1,4 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+//querySelecter("") - finds the FIRST query by name at that label
+//getElementsByTagName("") - finds all the queries by name at that label and it returns an array, for which we have to provide the index to which we want to make any change regarding style
+//like - document.getElementsByName("li")[0].style.color = "purple";, here the [] contains the index
