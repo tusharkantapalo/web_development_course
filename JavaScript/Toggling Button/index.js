eb7c956b86ee9a867a3467153c4054e025dcac7d
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-07-26 20:15:26 +0530

    added functioning button by js

--- JavaScript/Toggling Button/index.js
+++ JavaScript/Toggling Button/index.js
@@ -2,3 +2,4 @@ document.querySelector("button").classList.add("invisible");    //it adds the cl
 document.querySelector("button").classList.remove("invisible"); //it removes the class from the tag
 document.querySelector("button").classList.toggle("invisible"); //it toggles the class invisible
 //it works in chrome developer tool console
+document.querySelector("h1").classList.toggle("huge");
