4c76fc62b12d30f7ea32fb3e1e777ebde54fdb12
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-07-26 20:01:24 +0530

    added toggling of button by js

---
+++ JavaScript/Toggling Button/index.js
@@ -0,0 +1,3 @@
+document.querySelector("button").classList.add("invisible");    //it adds the class invisible to the tag
+document.querySelector("button").classList.remove("invisible"); //it removes the class from the tag
+document.querySelector("button").classList.toggle("invisible"); //it toggles the class invisible
