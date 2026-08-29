6c1f17bedf6e1bbd240617fe66d05a29f15047e8
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-08-29 22:36:06 +0530

    optimised the code

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -2,6 +2,6 @@ function handle() {
     alert("Clicked");
 }
 
-for(var i = 0; i < 7; i++){
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handle);
 }
