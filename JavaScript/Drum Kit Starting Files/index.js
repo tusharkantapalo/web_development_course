d4f862c6a2fc025a9afdb70da0353fe1f674a613
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-08-30 19:53:37 +0530

    a diff way of adding event_listners

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -4,4 +4,10 @@ function handle() {
 
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handle);
+    /*
+    document.querySelectorAll(".drum")[i].addEventListener("click", function()){
+        alert("Clicked");
+    }
+    */
+   //We can also add the eventlistner, by the commented way also.
 }
