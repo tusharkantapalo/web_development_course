e439b7ed3a45aca1617cfe342884bd694768b200
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-08-31 15:50:33 +0530

    changing coloe of every botton on each click

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -3,11 +3,13 @@ function handle() {
 }
 
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", handle);
-    /*
-    document.querySelectorAll(".drum")[i].addEventListener("click", function()){
-        alert("Clicked");
-    }
-    */
-   //We can also add the eventlistner, by the commented way also.
+    /*document.querySelectorAll(".drum")[i].addEventListener("click", handle);*/
+    //We can also add the eventlistner, by the commented way also.
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        //alert("Clicked");
+        var audio = new Audio("sounds/tom-1.mp3");    
+        audio.play();
+        this.style.color = "white";
+    })//IMPORTANT
 }
