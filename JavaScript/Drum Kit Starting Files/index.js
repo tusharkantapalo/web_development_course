b55d235a713bac83bb1141b41331d1d845d24633
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-09-06 10:55:17 +0530

    added to recognise keypress operations

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -1,45 +1,46 @@
-function handle() {
-    alert("Clicked");
+function keyPress(key) {
+    switch(key) {
+        case 'w':
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a' :
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case 's' :
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case 'd' :
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case 'j' :
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case 'k' :
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case 'l' :
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            alert("Key press is not identified!");
+    }
 }
 
-for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
-    /*document.querySelectorAll(".drum")[i].addEventListener("click", handle);*/
-    //We can also add the eventlistner, by the commented way also.
-    
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        //alert("Clicked");
-        // var audio = new Audio("sounds/tom-1.mp3");    
-        // audio.play();
-        // this.style.color = "white";
-        switch(this.innerHTML) {
-            case 'w':
-                var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
-                break;
-            case 'a': 
-                var audio = new Audio("sounds/tom-2.mp3");
-                audio.play();
-                break;
-            case 's': 
-                var audio = new Audio("sounds/tom-3.mp3");
-                audio.play();
-                break;
-            case 'd': 
-                var audio = new Audio("sounds/tom-4.mp3");
-                audio.play();
-                break;
-            case 'j': 
-                var audio = new Audio("sounds/snare.mp3");
-                audio.play();
-                break;
-            case 'k': 
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
-                break;
-            case 'l': 
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
-                break;
-        }
-    })//IMPORTANT
+//identifies the mouse clicks
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        keyPress(this.innerHTML);
+    });
 }
+
+//identifies the keyboard key presses
+document.addEventListener("keypress", function(event) {
+    keyPress(event.key);
+});
