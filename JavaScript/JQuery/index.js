3c232d2bf562e09168d2e310f15baa2d9925a195
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-09-07 22:34:33 +0530

    added multiple classes to html attribute

--- JavaScript/JQuery/index.js
+++ JavaScript/JQuery/index.js
@@ -4,4 +4,5 @@
 $("h1").css("color", "red");        //setting the color
 console.log($("h1").css("color"));  //getting the value
 
-$("big").addClass("big");           //adding a class to html
+$("h1").addClass("big");            //adding a class to html
+$("h1").addClass("big margine");    //adding multiple classes to any attribute
