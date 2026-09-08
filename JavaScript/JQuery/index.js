b172b765889eaccf51bb0e5dcda0a391a5bd3dee
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-09-08 10:41:17 +0530

    adding after

--- JavaScript/JQuery/index.js
+++ JavaScript/JQuery/index.js
@@ -28,3 +28,4 @@ $("h1").on("mouseover", function() {                    //another way of adding 
 });
 
 $("h1").before("<button>new</button>");                 //adding button before h1
+$("h1").after("<button>new</button>");                  //adding button before h1
